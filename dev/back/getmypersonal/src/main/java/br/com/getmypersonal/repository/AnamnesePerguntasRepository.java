5ec874fb3aabee0f8b7903396cddcabe52dca97a
Gustavo Assalin <gustavo.assalin@hotmail.com>
2025-04-06 00:28:37 -0300

    Criacao repository para perguntas anamnese

---
+++ dev/back/getmypersonal/src/main/java/br/com/getmypersonal/repository/AnamnesePerguntasRepository.java
@@ -0,0 +1,9 @@
+package br.com.getmypersonal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.getmypersonal.model.AnamnesePerguntas;
+
+public interface AnamnesePerguntasRepository extends JpaRepository<AnamnesePerguntas, Long> {
+
+}
