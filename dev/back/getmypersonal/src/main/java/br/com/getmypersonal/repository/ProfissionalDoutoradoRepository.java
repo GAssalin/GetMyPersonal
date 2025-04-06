64f70e8055055f139f2460069907bb34b8d2b323
Gustavo Assalin <gustavo.assalin@hotmail.com>
2025-04-06 20:10:25 -0300

    Correao repository

--- dev/back/getmypersonal/src/main/java/br/com/getmypersonal/repository/ProfissionalDoutoradoRepository.java
+++ dev/back/getmypersonal/src/main/java/br/com/getmypersonal/repository/ProfissionalDoutoradoRepository.java
@@ -3,7 +3,9 @@ package br.com.getmypersonal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.getmypersonal.model.ProfissionalDoutorado;
+
 @Repository
-public interface ProfissionalDoutorado extends JpaRepository<ProfissionalDoutorado, Long> {
+public interface ProfissionalDoutoradoRepository extends JpaRepository<ProfissionalDoutorado, Long> {
 
 }
--- dev/back/getmypersonal/src/main/java/br/com/getmypersonal/repository/ProfissionalMestradoRepository.java
+++ dev/back/getmypersonal/src/main/java/br/com/getmypersonal/repository/ProfissionalMestradoRepository.java
@@ -3,7 +3,9 @@ package br.com.getmypersonal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.getmypersonal.model.ProfissionalMestrado;
+
 @Repository
-public interface ProfissionalMestrado extends JpaRepository<ProfissionalMestrado, Long> {
+public interface ProfissionalMestradoRepository extends JpaRepository<ProfissionalMestrado, Long> {
 
 }
